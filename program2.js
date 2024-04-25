de3aca9f11b0d7e031993653dfb56362de652e9b
GovindaGupta28 <ag6420564@gmail.com>
2024-04-25 13:47:00 +0530

    Auto commit on save

--- program2.js
+++ program2.js
@@ -1,5 +1,4 @@
 function smallest_missing_positive_integer(nums) {
-
   nums = nums.filter((num) => num > 0);
   const numSet = new Set(nums);
   let smallestMissing = 1;
@@ -10,6 +9,6 @@ function smallest_missing_positive_integer(nums) {
   return smallestMissing;
 }
 
-console.log(smallest_missing_positive_integer([3, 4, -1, 1])); 
-console.log(smallest_missing_positive_integer([1, 2, 0])); 
+console.log(smallest_missing_positive_integer([3, 4, -1, 1]));
+console.log(smallest_missing_positive_integer([1, 2, 0]));
 console.log(smallest_missing_positive_integer([-1, -3, 4, 2]));
